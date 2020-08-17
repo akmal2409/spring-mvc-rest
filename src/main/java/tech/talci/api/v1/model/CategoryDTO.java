3805d52fdfd35f2d6104760e1143600c51328312
akmal2409 <akmalalikhujaev@gmail.com>
2020-08-17 16:59:35 +0200

    Added DTO Mapper via MapStruct

--- src/main/java/tech/talci/api/v1/model/CategoryDTO.java
+++ src/main/java/tech/talci/api/v1/model/CategoryDTO.java
@@ -1,5 +1,10 @@
 package tech.talci.api.v1.model;
 
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
