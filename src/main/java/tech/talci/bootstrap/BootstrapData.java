b956889712153074aad1eb1964ea6b8653402f60
akmal2409 <akmalalikhujaev@gmail.com>
2020-08-18 14:59:52 +0200

    Update Bootstrap Data and refactored it

--- src/main/java/tech/talci/bootstrap/BootstrapData.java
+++ src/main/java/tech/talci/bootstrap/BootstrapData.java
@@ -1,23 +1,34 @@
 package tech.talci.bootstrap;
 
 import lombok.extern.apachecommons.CommonsLog;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import tech.talci.domain.Category;
+import tech.talci.domain.Customer;
 import tech.talci.repositories.CategoryRepository;
+import tech.talci.repositories.CustomerRepository;
 
 @Component
+@Slf4j
 public class BootstrapData implements CommandLineRunner {
 
     private CategoryRepository categoryRepository;
+    private CustomerRepository customerRepository;
 
-    public BootstrapData(CategoryRepository categoryRepository) {
+    public BootstrapData(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        loadCategories();
+        loadCustomers();
+    }
+
+    public void loadCategories() {
         Category fruits = new Category();
         fruits.setName("Fruits");
 
@@ -39,6 +50,28 @@ public class BootstrapData implements CommandLineRunner {
         categoryRepository.save(exotic);
         categoryRepository.save(nuts);
 
-        System.out.println("Data loaded: " + categoryRepository.count());
+        log.debug("Categories were loaded: " + categoryRepository.count());
+    }
+
+    public void loadCustomers() {
+        Customer jack = new Customer();
+        Customer fiona = new Customer();
+        Customer akmal = new Customer();
+
+        jack.setId(1L);
+        jack.setFirstName("Jack");
+        jack.setFirstName("Barker");
+        fiona.setId(2L);
+        fiona.setFirstName("Fiona");
+        fiona.setLastName("Shrek");
+        akmal.setId(3L);
+        akmal.setFirstName("Aki");
+        akmal.setLastName("Test");
+
+        customerRepository.save(jack);
+        customerRepository.save(fiona);
+        customerRepository.save(akmal);
+
+        log.debug("Customers were loaded: " + customerRepository.count());
     }
 }
