e8eadbd0357485340e8373ecf7c519f0b0976659
akmal2409 <akmalalikhujaev@gmail.com>
2020-08-19 15:29:46 +0200

    Implemented VendorServiceImpl, passed all tests

--- src/main/java/tech/talci/services/VendorServiceImpl.java
+++ src/main/java/tech/talci/services/VendorServiceImpl.java
@@ -3,9 +3,11 @@ package tech.talci.services;
 import org.springframework.stereotype.Service;
 import tech.talci.api.v1.mapper.VendorMapper;
 import tech.talci.api.v1.model.VendorDTO;
+import tech.talci.exceptions.ResourceNotFoundException;
 import tech.talci.repositories.VendorRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VendorServiceImpl implements VendorService {
@@ -20,11 +22,28 @@ public class VendorServiceImpl implements VendorService {
 
     @Override
     public List<VendorDTO> getAllVendors() {
-        return null;
+        return vendorRepository.findAll()
+                                .stream()
+                                .map(vendor -> {
+                                    VendorDTO vendorDTO = vendorMapper.vendorToVendorDTO(vendor);
+                                    vendorDTO.setVendorUrl(getVendorUrl(vendor.getId()));
+                                    return vendorDTO;
+                                })
+                                .collect(Collectors.toList());
     }
 
     @Override
     public VendorDTO findById(Long id) {
-        return null;
+        return vendorRepository.findById(id)
+                .map(vendor -> {
+                    VendorDTO vendorDTO = vendorMapper.vendorToVendorDTO(vendor);
+                    vendorDTO.setVendorUrl(getVendorUrl(vendor.getId()));
+                    return vendorDTO;
+                })
+                .orElseThrow(() -> new ResourceNotFoundException("Vendor not found ID: " + id));
+    }
+
+    public String getVendorUrl(Long id){
+        return "/api/vendors/" + id;
     }
 }
